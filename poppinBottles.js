0ba2a0ef24ce65f145da6d0f03438f6b8516baca
Johannes Weber <johannes@drweber.de>
2021-11-03 17:11:49 -0700

    added getBottlesFromBottles

--- poppinBottles.js
+++ poppinBottles.js
@@ -1,8 +1,25 @@
 const poppinBottles = () => {
+  /*
+
+
+    calcTotalBottles(amount)
+
+      Calculate # of bottles purchased directly
+        Calculate # of bottles earned through recycling bottles
+        Calculate # of bottles earned through recycling bottle caps
+
+  */
 
 
 
 
+
+};
+
+const getBottlesFromBottles = (n) => {
+  const redeemedBottles = Math.floor(n / 2);
+  const result = redeemedBottles + getBottlesFromBottles(redeemedBottles);
+  return result;
 };
 
 module.exports = poppinBottles;
