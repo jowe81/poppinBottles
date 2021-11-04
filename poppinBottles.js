1ed88422a4494d4c2b46d38ab96421f5f8c59ed5
Johannes Weber <johannes@drweber.de>
2021-11-03 18:00:59 -0700

    Strategy change

--- poppinBottles.js
+++ poppinBottles.js
@@ -5,8 +5,10 @@ const poppinBottles = () => {
     calcTotalBottles(amount)
 
       Calculate # of bottles purchased directly
-        Calculate # of bottles earned through recycling bottles
-        Calculate # of bottles earned through recycling bottle caps
+      
+      Calculate # of bottles earned through recycling bottles (recursively)
+      
+      Calculate # of bottles earned through recycling all the caps
 
   */
 
@@ -17,8 +19,13 @@ const poppinBottles = () => {
 };
 
 /*
-  Calculate bottles redeemed directly
-  Calculate leftover
+  Calculate bottles redeemed directly (from 10)
+    From bottles (5/0)
+    From caps (2/2)
+
+  Calculate leftover bottles
+  Calculate leftover caps
+
   If leftover + redeemed directly >= 2
     result = redeemed directly + getBottlesFromBottles(redeemed directly + leftover)
 */
@@ -34,7 +41,41 @@ const getBottlesFromBottles = (n) => {
   return result;
 };
 
+// Same principle as getBottlesFromBottles
+const getBottlesFromCaps = (n) => {
+  const redeemedBottles = Math.floor(n / 4);
+  const leftOverCaps = n % 4;
+  if (redeemedBottles + leftOverBottles >= 2) {
+    result = redeemedBottles + getBottlesFromBottles(redeemedBottles + leftOverBottles);
+  } else {
+    result = redeemedBottles;
+  }
+  let result;
+}
+
+
+const recycle = (bottles, caps) => {
+  let result = {};
+  if (bottles < 2 && caps < 4) {
+    //Not enough bottles or caps to redeem any more bottles
+    result.newBottles = 0;
+    result.leftOverCaps = caps;
+    result.leftOverBottles = bottles;
+  } else {
+    //Recycle this round
+    let newBottlesFromBottles = Math.floor(bottles / 2);
+    let newBottlesFromCaps = Math.floor(caps / 4);
+    result.newBottles = newBottlesFromBottles + newBottlesFromCaps;
+    result.leftOverCaps = caps % 4;
+    result.leftOverBottles = bottles % 2;  
+  }
+  return result;
+};
+
+console.log(recycle(10,10));
+
 module.exports = {
   getBottlesFromBottles: getBottlesFromBottles,
   poppinBottles: poppinBottles,
+  recycle: recycle
 };
--- test/poppinBottles.test.js
+++ test/poppinBottles.test.js
@@ -17,6 +17,15 @@ describe("poppinBottles", () => {
 
     });
 
+    describe("recycle", () => {
+
+      it("should return 7 new bottles, 2 leftover caps, 0 leftover bottles for 10 recycled bottles and 10 recycled caps", () =>{
+        const result = poppinBottles.recycle(10, 10);
+        assert.equal(result.newBottles === 7 && result.leftOverCaps === 2 && result.leftOverBottles === 0, true);
+      });
+
+    });
+
   });
 
 });
