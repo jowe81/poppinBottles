b581d363f83a4e72a865ac8042d994cfe247b438
Johannes Weber <johannes@drweber.de>
2021-11-03 18:33:35 -0700

    Task 2 done and some refactoring

--- poppinBottles.js
+++ poppinBottles.js
@@ -1,41 +1,8 @@
-/*
-  Calculate new bottles, leftover caps, and leftover bottles
-  for one set of recycling input data (bottles, caps)
-*/
-const recycle = (bottles, caps) => {
-  let result = {};
-  let newBottlesFromBottles = Math.floor(bottles / 2);
-  let newBottlesFromCaps = Math.floor(caps / 4);
-  result.newBottles = newBottlesFromBottles + newBottlesFromCaps;
-  result.leftOverCaps = caps % 4;
-  result.leftOverBottles = bottles % 2;
-  return result;
-};
+const recycling = require('./recycling');
 
-
-/*
-  Purchase bottles from money
-  do
-    recycle empty bottles and caps
-  while (there are at least 2 bottles or at least 4 caps to recycle)
-*/
-const calculateTotalReturn = (amount) => {
-  let bottles = Math.floor(amount / 2);
-  let caps = bottles;
-  let thisRoundResult;
-  let totalBottles = bottles;
-  //console.log("initial bottles",bottles," and caps ",caps);
-  do {
-    thisRoundResult = recycle(bottles, caps);
-    totalBottles += thisRoundResult.newBottles;
-    bottles = thisRoundResult.newBottles + thisRoundResult.leftOverBottles;
-    caps = thisRoundResult.newBottles + thisRoundResult.leftOverCaps;
-    //console.log ("this round result:",thisRoundResult);
-  } while (bottles >= 2 || caps >= 4);
-  return totalBottles;
-};
-
-module.exports = {
-  recycle: recycle,
-  calculateTotalReturn: calculateTotalReturn,
-};
+if (process.argv.length > 2) {
+  amount = process.argv[2];
+  console.log("TOTAL BOTTLES:",recycling.calculateTotalReturn(amount));
+} else {
+  console.log("No amount provided");
+}
--- test/recycling.test.js
+++ test/recycling.test.js
@@ -1,22 +1,22 @@
 const assert = require('assert').strict;
-const poppinBottles = require('../poppinBottles');
+const recycling = require('../recycling');
 
-describe("poppinBottles", () => {
+describe("recycling library", () => {
 
   describe("helper functions", () => {
 
     describe("calculateTotalReturn (dollar amount)", () => {
 
       it("should return 15 for 10", () => {
-        assert.equal(poppinBottles.calculateTotalReturn(10), 15);
+        assert.equal(recycling.calculateTotalReturn(10), 15);
       });
 
       it("should return 35 for 20", () => {
-        assert.equal(poppinBottles.calculateTotalReturn(20), 35);
+        assert.equal(recycling.calculateTotalReturn(20), 35);
       });
 
       it("should return 75 for 40", () => {
-        assert.equal(poppinBottles.calculateTotalReturn(40), 75);
+        assert.equal(recycling.calculateTotalReturn(40), 75);
       });
 
     });
@@ -24,7 +24,7 @@ describe("poppinBottles", () => {
     describe("recycle", () => {
 
       it("should return 7 new bottles, 2 leftover caps, 0 leftover bottles for 10 recycled bottles and 10 recycled caps", () =>{
-        const result = poppinBottles.recycle(10, 10);
+        const result = recycling.recycle(10, 10);
         assert.equal(result.newBottles === 7 && result.leftOverCaps === 2 && result.leftOverBottles === 0, true);
       });
 
